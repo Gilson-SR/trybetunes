b419aa1483e7182ee717dcb63374d96844bbe416
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-09-05 15:51:06 -0300

    Criação dos links de navegação do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../Loading';
 import { getUser } from '../services/userAPI';
 
@@ -28,6 +29,16 @@ export default class Header extends React.Component {
         ) : (
           <p data-testid="header-user-name">{user.name}</p>
         )}
+
+        <Link to="/search" data-testid="link-to-search">
+          Search
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favorites
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Profile
+        </Link>
       </header>
     );
   }
