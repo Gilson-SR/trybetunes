0a857140cefd398bcc237939b0bb9a1d0abac39f
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-09-05 16:07:07 -0300

    Crição do formulário para buscar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
 export default class Search extends React.Component {
+  state = {
+    artist: '',
+  };
+
   render() {
+    const { artist } = this.state;
     return (
-      <div data-testid="page-search">Search</div>
+      <div data-testid="page-search">
+        Search
+        <input
+          type="text"
+          name="artist"
+          id="inputArtist"
+          data-testid="search-artist-input"
+          onChange={ ({ target: { value } }) => this.setState({ artist: value }) }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ artist.length < 2 }
+        >
+          Pesquisar
+        </button>
+      </div>
     );
   }
 }
